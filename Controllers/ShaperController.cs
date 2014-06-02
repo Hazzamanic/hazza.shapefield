a5e65d88f5a10e3d283c5a99f1cc0066a89d50f6
Hazza <harry@modstreaming.com>
2014-06-02 15:02:31 +0100

    initial commit

---
+++ Controllers/ShaperController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using Orchard;
+using Orchard.DisplayManagement;
+using Orchard.Themes;
+
+
+namespace Hazza.ShapeField.Controllers
+{
+    public class ShaperController : Controller
+    {
+        private readonly IShapeDisplay shapeDisplay;
+
+        public ShaperController(IShapeFactory shapeFactory, IShapeDisplay shapeDisplay)
+        {
+            Shape = shapeFactory;
+            this.shapeDisplay = shapeDisplay;
+        }
+
+        dynamic Shape { get; set; }
+
+        // for some reason this doesn't work, in the "callsite" bit returns the shape as null. works fine in other places...
+        public ActionResult TestShape(string shape)
+        {
+            var test = Shape.Create(shape);
+            var html = shapeDisplay.Display(shape);
+            return Json(html, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Fields/ShapeField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.FieldStorage;
+
+namespace Hazza.ShapeField.Fields
+{
+    public class ShapeField : ContentField
+    {
+        public string Value {
+            get { return Storage.Get<string>(); }
+            set { Storage.Set(value ?? String.Empty); }
+        }
+    }
+}
